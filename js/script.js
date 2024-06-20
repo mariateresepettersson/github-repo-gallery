3119617f97c4852d75402c68c61afdc739dfe1d7
Maria Pettersson <mariateresepettersson@gmail.com>
2024-06-20 07:43:45 +0200

    Declare a function to display the fetched user information on the page

--- js/script.js
+++ js/script.js
@@ -6,5 +6,24 @@ const getUserData = async function () {
   const res = await fetch(`https://api.github.com/users/${user}`);
   const data = await res.json();
   console.log(data);
+  displayUserData(data);
 };
 getUserData();
+
+//Declare a function to display the fetched user information on the page
+const displayUserData = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> 
+    `;
+  overview.append(div);
+};
