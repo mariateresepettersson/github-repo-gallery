0120914d901af00878ab259762c6bce9a42e4e2f
Maria Pettersson <mariateresepettersson@gmail.com>
2024-06-20 07:20:05 +0200

    Declare a function to fetch user data from git

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 const overview = document.querySelector(".overview");
+const user = "mariateresepettersson";
+
+//Declare a function to fetch user data from git
+const getUserData = async function () {
+  const res = await fetch(`https://api.github.com/users/${user}`);
+  const data = await res.json();
+  console.log(data);
+};
+getUserData();
